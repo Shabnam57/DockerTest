3e623b71d8d635acffde2b5debf4e84ca2b73f32
LAPTOP-V2C8J1MU\Shabnam <f.alhavan@gmail.com>
2018-02-13 14:07:34 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//@ts-check
+
+var http = require("http");
+var fs = require("fs");
+
+http.createServer((req,res)=>{
+    if(req.url === "/index"){
+        res.writeHead(200,"text/plain");
+        res.end("Hello World");
+    }else if(req.url === "/help")
+    {
+        res.writeHead(200,"text/html");
+        fs.createReadStream(__dirname + "/help.htm").pipe(res);
+    }
+    else
+    {
+        res.writeHead(404,{"contect-type" : "json"},"utf8");
+        var str = {
+            errorName : "not Found"
+        };
+        res.end(JSON.stringify( str));
+    }
+}).listen("3000","127.0.0.1",function(){
+ console.log("listening ...");
+});
